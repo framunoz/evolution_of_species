5f64dc48216a42524ea527e5896ae8b90c7be6ad
Francisco Muñoz <femunoz@dim.uchile.cl>
2021-06-11 10:47:03 -0400

    feat: Agregados requirements.txt y setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="perthame_edp",
+    packages=find_packages(),
+    version="0.0.1",
+    description="Library to solve the Perthame's equations",
+    author="J. Bravo, V. Gómez & F. Muñoz",
+    license=license()
+)
