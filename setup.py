ef7067cc38450f9878157ee95b7b17bf0ad86dc6
Francisco Muñoz <femunoz@dim.uchile.cl>
2021-06-11 11:27:46 -0400

    docs: Actualizada setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
+import pathlib
+
+import pkg_resources
 from setuptools import find_packages, setup
+
+# List of requirements
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
     name="perthame_edp",
     packages=find_packages(),
-    version="0.0.1",
+    version="0.1.0",
     description="Library to solve the Perthame's equations",
     author="J. Bravo, V. Gómez & F. Muñoz",
-    license=license()
+    # license=None,
+    install_requires=install_requires,
 )
